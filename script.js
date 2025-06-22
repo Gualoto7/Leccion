2c452a864c1b5a7ee5feca7af6b933427c17da56
Gualoto7 <lgualoto70@gmail.com>
2025-06-21 19:58:07 -0500

    Se agrega movimiento de la serpiente con flechas

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const ctx = canvas.getContext("2d");
 
 const gridSize = 20;
 const snake = [{ x: 10, y: 10 }];
+let dx = 0;
+let dy = 0;
 
 function drawBackground() {
   ctx.fillStyle = "#000";
@@ -16,5 +18,25 @@ function drawSnake() {
   }
 }
 
-drawBackground();
-drawSnake();
+document.addEventListener("keydown", (e) => {
+  if (e.key === "ArrowUp") {
+    dx = 0; dy = -1;
+  } else if (e.key === "ArrowDown") {
+    dx = 0; dy = 1;
+  } else if (e.key === "ArrowLeft") {
+    dx = -1; dy = 0;
+  } else if (e.key === "ArrowRight") {
+    dx = 1; dy = 0;
+  }
+});
+
+function gameLoop() {
+  const head = { x: snake[0].x + dx, y: snake[0].y + dy };
+  snake.unshift(head);
+  snake.pop();
+
+  drawBackground();
+  drawSnake();
+}
+
+setInterval(gameLoop, 150);
