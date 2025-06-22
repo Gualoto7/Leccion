b681634424f2f9ce68e5639c9fa5e4f5f4fd2ff4
Gualoto7 <lgualoto70@gmail.com>
2025-06-21 19:56:58 -0500

    Se dibuja la serpiente en la posición inicial

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
 const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
 
+const gridSize = 20;
+const snake = [{ x: 10, y: 10 }];
+
 function drawBackground() {
   ctx.fillStyle = "#000";
   ctx.fillRect(0, 0, canvas.width, canvas.height);
 }
 
+function drawSnake() {
+  ctx.fillStyle = "#0f0";
+  for (let part of snake) {
+    ctx.fillRect(part.x * gridSize, part.y * gridSize, gridSize, gridSize);
+  }
+}
+
 drawBackground();
+drawSnake();
